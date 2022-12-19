46c9060c3cd5c98c87d127b84ceaae730bd4759d
Appieasahbie | AkNodes <108979536+appieasahbie@users.noreply.github.com>
2022-12-19 21:48:24 +0100

    Update massa.sh

--- massa.sh
+++ massa.sh
@@ -10,7 +10,7 @@ echo "       ####      ####    ####        ####        ####   #########";
 echo "      ####        ####   ####        ####        ####   #########";
 
 echo -e '\e[36mTwitter:\e[39m' https://twitter.com/ABDERRAZAKAKRI3
-echo -e '\e[36mGithub: \e[39m' https://github.com/appieasahbie
+echo -e '\e[36mGithub: \e[39m' https://aknodes.com/
 echo -e "\e[0m"
 
 
