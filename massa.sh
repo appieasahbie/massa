64878388de76f2fa36a06cb2e0b93b4940914944
Appieasahbie | AkNodes <108979536+appieasahbie@users.noreply.github.com>
2022-12-19 21:41:48 +0100

    Update massa.sh

--- massa.sh
+++ massa.sh
@@ -14,118 +14,75 @@ echo -e '\e[36mGithub: \e[39m' https://github.com/appieasahbie
 echo -e "\e[0m"
 
 
-
 sleep 2
 
-# set vars
-if [ ! $NODENAME ]; then
-	read -p "Enter node name: " NODENAME
-	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
-fi
-HAQQ_PORT=35
-if [ ! $WALLET ]; then
-	echo "export WALLET=wallet" >> $HOME/.bash_profile
-fi
-echo "export HAQQ_CHAIN_ID=haqq_54211-2" >> $HOME/.bash_profile
-echo "export HAQQ_PORT=${HAQQ_PORT}" >> $HOME/.bash_profile
-source $HOME/.bash_profile
+bold=$(tput bold)
+underline=$(tput smul)
+italic=$(tput sitm)
+info=$(tput setaf 2)
+error=$(tput setaf 160)
+warn=$(tput setaf 214)
+reset=$(tput sgr0)
 
-echo '================================================='
-echo -e "Your node name: \e[1m\e[32m$NODENAME\e[0m"
-echo -e "Your wallet name: \e[1m\e[32m$WALLET\e[0m"
-echo -e "Your chain name: \e[1m\e[32m$HAQQ_CHAIN_ID\e[0m"
-echo -e "Your port: \e[1m\e[32m$HAQQ_PORT\e[0m"
-echo '================================================='
+clear;
+echo "*********************---*********************"
+echo "*******Altai - Massa Node Installation*******"
+echo "*********************---*********************"
+sleep 3
+
+# server update and port settings
+sudo apt-get update -y
+sudo apt install ufw -y
+sudo ufw allow 22
+sudo ufw allow ssh
+sudo ufw allow 31244/tcp
+sudo ufw allow 31245/tcp
+sudo ufw enable
+
+
+clear;
+sudo ufw status
+echo "${info}INFO${reset}: installation is ${bold}set${reset} please wait... "
+
+sleep 5
+
+# required libraries
+sudo apt install pkg-config curl git build-essential libssl-dev libclang-dev -y
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source $HOME/.cargo/env
+rustc --version
+rustup toolchain install nightly
+rustup default nightly
+rustc --version
+git clone --branch testnet https://github.com/massalabs/massa.git
+
+# rustc explain fixed
+sudo apt install make clang pkg-config libssl-dev -y
+rustup default nightly 
+rustup update
+
+# settings file
+clear;
+echo "---------------------"
+echo "${info}INFO${reset}: Libraries loaded ${bold}successfully${reset}..."
+echo "---------------------"
 sleep 2
 
-echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
-# update
-sudo apt update && sudo apt upgrade -y
+echo "Enter your server's ip address :"
+read ipadr
+echo -e "[network]\nroutable_ip = '$ipadr'" >> massa/massa-node/config/config.toml
 
-echo -e "\e[1m\e[32m2. Installing dependencies... \e[0m" && sleep 1
-# packages
-sudo apt install curl build-essential git wget jq make gcc tmux chrony -y
+echo "Set node and client password: :"
+read walletpassword
 
-# install go
-if ! [ -x "$(command -v go)" ]; then
-  ver="1.18.2"
-  cd $HOME
-  wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-  sudo rm -rf /usr/local/go
-  sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-  rm "go$ver.linux-amd64.tar.gz"
-  echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
-  source ~/.bash_profile
-fi
+sudo apt install screen -y
 
-echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
-# download binary
-cd $HOME
-git clone https://github.com/haqq-network/haqq.git
-cd haqq
-git checkout 1.2.0
-make install
+# node start
+screen -S massa-node -d -m bash
+screen -r massa-node -X stuff "cd massa/massa-node/ && RUST_BACKTRACE=full cargo run --release -- -p $walletpassword |& tee logs.txt"$(echo -ne '\015')
+echo "${info}INFO${reset}: NODE ${bold}STARTED${reset}."
 
-# config
-haqqd config chain-id $HAQQ_CHAIN_ID
-haqqd config keyring-backend test
-haqqd config node tcp://localhost:${HAQQ_PORT}657
-
-# init
-haqqd init $NODENAME --chain-id $HAQQ_CHAIN_ID
-
-# download genesis and addrbook
-wget -qO $HOME/.haqqd/config/genesis.json "https://raw.githubusercontent.com/haqq-network/validators-contest/master/genesis.json"
-
-# set peers and seeds
-SEEDS="62bf004201a90ce00df6f69390378c3d90f6dd7e@seed2.testedge2.haqq.network:26656,23a1176c9911eac442d6d1bf15f92eeabb3981d5@seed1.testedge2.haqq.network:26656"
-PEERS="b3ce1618585a9012c42e9a78bf4a5c1b4bad1123@65.21.170.3:33656,952b9d918037bc8f6d52756c111d0a30a456b3fe@213.239.217.52:29656,85301989752fe0ca934854aecc6379c1ccddf937@65.109.49.111:26556,d648d598c34e0e58ec759aa399fe4534021e8401@109.205.180.81:29956,f2c77f2169b753f93078de2b6b86bfa1ec4a6282@141.95.124.150:20116,eaa6d38517bbc32bdc487e894b6be9477fb9298f@78.107.234.44:45656,37513faac5f48bd043a1be122096c1ea1c973854@65.108.52.192:36656,d2764c55607aa9e8d4cee6e763d3d14e73b83168@66.94.119.47:26656,fc4311f0109d5aed5fcb8656fb6eab29c15d1cf6@65.109.53.53:26656,297bf784ea674e05d36af48e3a951de966f9aa40@65.109.34.133:36656,bc8c24e9d231faf55d4c6c8992a8b187cdd5c214@65.109.17.86:32656"
-sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.haqqd/config/config.toml
-
-# set custom ports
-sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${HAQQ_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${HAQQ_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${HAQQ_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${HAQQ_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${HAQQ_PORT}660\"%" $HOME/.haqqd/config/config.toml
-sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${HAQQ_PORT}317\"%; s%^address = \":8080\"%address = \":${HAQQ_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${HAQQ_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${HAQQ_PORT}091\"%" $HOME/.haqqd/config/app.toml
-
-# config pruning
-pruning="custom"
-pruning_keep_recent="100"
-pruning_keep_every="0"
-pruning_interval="50"
-sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.haqqd/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.haqqd/config/app.toml
-sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.haqqd/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.haqqd/config/app.toml
-
-# set minimum gas price and timeout commit
-sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0aISLM\"/" $HOME/.haqqd/config/app.toml
-
-# enable prometheus
-sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.haqqd/config/config.toml
-
-# reset
-haqqd tendermint unsafe-reset-all --home $HOME/.haqqd
-
-echo -e "\e[1m\e[32m4. Starting service... \e[0m" && sleep 1
-# create service
-sudo tee /etc/systemd/system/haqqd.service > /dev/null <<EOF
-[Unit]
-Description=haqq
-After=network-online.target
-[Service]
-User=$USER
-ExecStart=$(which haqqd) start --home $HOME/.haqqd
-Restart=on-failure
-RestartSec=3
-LimitNOFILE=65535
-[Install]
-WantedBy=multi-user.target
-EOF
-
-# start service
-sudo systemctl daemon-reload
-sudo systemctl enable haqqd
-sudo systemctl restart haqqd
-
-echo '=============== SETUP FINISHED ==================='
-echo -e 'To check logs: \e[1m\e[32mjournalctl -u haqqd -f -o cat\e[0m'
-echo -e "To check sync status: \e[1m\e[32mcurl -s localhost:${HAQQ_PORT}657/status | jq .result.sync_info\e[0m"
+# client start
+screen -S massa-client -d -m bash
+screen -r massa-client -X stuff "cd massa/massa-client/ && cargo run --release -- -p $walletpassword"$(echo -ne '\015')
+echo "${info}INFO${reset}: CLIENT ${bold}STARTED${reset}."
